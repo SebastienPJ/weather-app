b37f5a4317d17139d170e154344003c6b4daf609
Sebastien <sebastienpj@protonmail.com>
2021-12-26 20:55:12 -0600

    add user input to fetch data

--- scripts.js
+++ scripts.js
@@ -1,3 +1,64 @@
+let states = ["", "Alaska",
+              "Alabama",
+              "Arkansas",
+              "American Samoa",
+              "Arizona",
+              "California",
+              "Colorado",
+              "Connecticut",
+              "District of Columbia",
+              "Delaware",
+              "Florida",
+              "Georgia",
+              "Guam",
+              "Hawaii",
+              "Iowa",
+              "Idaho",
+              "Illinois",
+              "Indiana",
+              "Kansas",
+              "Kentucky",
+              "Louisiana",
+              "Massachusetts",
+              "Maryland",
+              "Maine",
+              "Michigan",
+              "Minnesota",
+              "Missouri",
+              "Mississippi",
+              "Montana",
+              "North Carolina",
+              " North Dakota",
+              "Nebraska",
+              "New Hampshire",
+              "New Jersey",
+              "New Mexico",
+              "Nevada",
+              "New York",
+              "Ohio",
+              "Oklahoma",
+              "Oregon",
+              "Pennsylvania",
+              "Puerto Rico",
+              "Rhode Island",
+              "South Carolina",
+              "South Dakota",
+              "Tennessee",
+              "Texas",
+              "Utah",
+              "Virginia",
+              "Virgin Islands",
+              "Vermont",
+              "Washington",
+              "Wisconsin",
+              "West Virginia",
+              "Wyoming"
+]
+
+
+            
+
+
 async function getData(city, state) {
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state}&units=imperial&appid=3ca02677eec4ac8a96141272e96128a2`, { mode: 'cors' })
@@ -18,9 +79,9 @@ function processData(data) {
   const cityName = data.name;
   const humidity = data.main.humidity;
   const feelsLike = data.main.feels_like;
-  const currentTemp = data.main.temp;
-  const maxTemp = data.main.temp_max;
-  const minTemp = data.main.temp_min;
+  const currentTemp = Math.floor(data.main.temp);
+  const maxTemp = Math.floor(data.main.temp_max);
+  const minTemp = Math.floor(data.main.temp_min);
 
   return { cityName, humidity, feelsLike, currentTemp, maxTemp, minTemp }
 
@@ -38,6 +99,14 @@ function renderToPage(dataObj) {
 }
 
 
+const statesTag = document.querySelector('#state')
+states.forEach(state => {
+  const newOption = document.createElement('option');
+  newOption.textContent = state
+  statesTag.appendChild(newOption)
+})
+
+
 const city = document.querySelector('.city')
 const temp = document.querySelector('.current-temp')
 const weatherDesciption = document.querySelector('.temp-description')
@@ -45,11 +114,29 @@ const icon = document.querySelector('.icon')
 const highTemp = document.querySelector('.high')
 const lowTemp = document.querySelector('.low')
 
+const form = document.querySelector(".form")
+const submitButton = document.querySelector('.submit')
+submitButton.addEventListener('click', function(e) {
+  e.preventDefault()
 
-const unprocessedData = getData('dallas', 'texas') 
+  const formData = new FormData(form)
 
-const weatherData = unprocessedData.then(data => processData(data))
-                      .then(obj => {
-                        renderToPage(obj)
-                      }).catch(err => console.error(err))
+  const cityChosen = formData.get('city')
+  const stateChosen = formData.get('state')
+
+  getData(cityChosen, stateChosen).then(unprocessedData => processData(unprocessedData))
+      .then(obj => {renderToPage(obj)})
+
+  form.reset();
+})
+
+// const unprocessedData = getData('dallas', 'texas') 
+
+// const weatherData = unprocessedData.then(data => processData(data))
+//                       .then(obj => {
+//                         renderToPage(obj)
+//                       }).catch(err => console.error(err))
+
+
+
 
